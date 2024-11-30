998b12fec442db59ea20ce2c9e38ba91624d6de5
caipeng <iiicp@outlook.com>
2024-11-30 10:37:31 +0800

    feat(frk): 1. add target machine

--- llvm/lib/Target/One/OneTargetMachine.cpp
+++ llvm/lib/Target/One/OneTargetMachine.cpp
@@ -3,11 +3,34 @@
 //
 
 #include "OneTargetMachine.h"
-#include "llvm/MC/TargetRegistry.h"
 #include "TargetInfo/OneTargetInfo.h"
+#include "llvm/CodeGen/TargetLoweringObjectFileImpl.h"
+#include "llvm/MC/TargetRegistry.h"
 
 using namespace llvm;
 
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeOneTarget() {
-
+  RegisterTargetMachine<OneTargetMachine> X(getTheOneTarget());
 }
+
+static StringRef computeDataLayout(const Triple &TT,
+                                   const TargetOptions &options) {
+  assert(TT.isArch32Bit() && "only 32 bit are currently supported");
+  return "e-m:e-p:32:32-i64:64-n32-S128";
+}
+
+static Reloc::Model getEffectiveRelocModel(const Triple &TT,
+                                           std::optional<Reloc::Model> RM) {
+  return RM.value_or(Reloc::Static);
+}
+
+OneTargetMachine::OneTargetMachine(const Target &T, const Triple &TT,
+                                   StringRef CPU, StringRef FS,
+                                   const TargetOptions &Options,
+                                   std::optional<Reloc::Model> RM,
+                                   std::optional<CodeModel::Model> CM,
+                                   CodeGenOptLevel OL, bool JIT)
+    : LLVMTargetMachine(T, computeDataLayout(TT, Options), TT, CPU, FS, Options,
+                        getEffectiveRelocModel(TT, RM),
+                        getEffectiveCodeModel(CM, CodeModel::Small), OL),
+      TLOF(std::make_unique<TargetLoweringObjectFileELF>()) {}
