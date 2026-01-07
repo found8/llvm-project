43b3098926f43d544e2e90c46595e037e5e5a0b2
Marvin.Li <marvin.li@vastaitech.com>
2026-01-07 18:26:23 +0800

    add nvvm_read_ptx_sreg_tid intrinsic support

--- llvm/lib/IR/Intrinsics.cpp
+++ llvm/lib/IR/Intrinsics.cpp
@@ -26,6 +26,7 @@
 #include "llvm/IR/IntrinsicsPowerPC.h"
 #include "llvm/IR/IntrinsicsR600.h"
 #include "llvm/IR/IntrinsicsRISCV.h"
+#include "llvm/IR/IntrinsicsRISCX.h"
 #include "llvm/IR/IntrinsicsS390.h"
 #include "llvm/IR/IntrinsicsVE.h"
 #include "llvm/IR/IntrinsicsX86.h"
